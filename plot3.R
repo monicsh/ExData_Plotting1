65e6ab9822cdabb16937840ef4b1d90c68e21ce6
Monika Sharma <monic.sh@gmail.com>
2014-05-11 18:20:27 -0500

    added par() and mar() to adust labels

--- plot3.R
+++ plot3.R
@@ -5,10 +5,14 @@ dataset <- read.table("household_power_consumption.txt",
            skip = 66637, nrow = 2880, sep = ";",
            col.names = colnames(read.table("household_power_consumption.txt", 
                                            nrow = 1, header = TRUE, sep = ";")))
+par(mfrow = c(1, 1), mar = c(4,4,2,2))
 
 ## Making Plot 3 
 wdays <- strptime(paste(dataset$Date, dataset$Time), format='%d/%m/%Y %H:%M:%S')
 plot(wdays, dataset$Sub_metering_1,  type="l", xlab="", ylab="Energy sub metering")
 with(dataset, points(wdays, dataset$Sub_metering_2,  type="l", xlab="", col = "red"))
 with(dataset, points(wdays, dataset$Sub_metering_3,  type="l", xlab="", col = "blue"))
-legend("topright", lty = 1:1,  col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright", lty = 1:1, col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1          ","Sub_metering_2","Sub_metering_3"),
+       bty = "o", cex = .75)
+
--- plot4.R
+++ plot4.R
@@ -8,14 +8,17 @@ dataset <- read.table("household_power_consumption.txt",
 
 ## Making plot 4
 wdays <- strptime(paste(dataset$Date, dataset$Time), format='%d/%m/%Y %H:%M:%S')
-par(mfrow = c(2, 2))
+par(mfrow = c(2, 2), mar = c(4,4,2,2))
 with(dataset, {
     plot(wdays, dataset$Global_active_power, type="l", xlab="", ylab="Global Active Power")
     plot(wdays, dataset$Voltage, type="l", xlab="datetime", ylab="Voltage")
     plot(wdays, dataset$Sub_metering_1,  type="l", xlab="", ylab="Energy sub metering")
     with(dataset, points(wdays, dataset$Sub_metering_2,  type="l", xlab="", col = "red"))
     with(dataset, points(wdays, dataset$Sub_metering_3,  type="l", xlab="", col = "blue"))
-    legend("topright", lty = 1:1,  col = c("black", "red", "blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+    legend("topright", lty = 1:1, col = c("black", "red", "blue"),
+           legend = c("Sub_metering_1          ","Sub_metering_2","Sub_metering_3"),
+           bty = "n", cex = .75)
+    
     plot(wdays, dataset$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
     
 })
